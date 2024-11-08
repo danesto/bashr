eef72fc308ca80601b6322333e3477228176da2d
Danilo Stojanovic <danilo@ip-192-168-0-20.eu-central-1.compute.internal>
2024-11-08 15:23:31 +0100

    add: drawer ui

--- components/app/test-cases/test-case-drawer.tsx
+++ components/app/test-cases/test-case-drawer.tsx
@@ -5,6 +5,7 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
+import Image from "next/image";
 
 type TestCaseDrawerProps = {
   testCase: {
@@ -26,14 +27,33 @@ export const TestCaseDrawer = ({
 
   return (
     <Sheet open={isOpen} onOpenChange={onOpenChange}>
-      <SheetContent className="dark text-white sm:w-[600px] sm:max-w-[1000px]">
-        <SheetHeader>
-          <SheetTitle>
-            {name} <span>{id}</span>
-          </SheetTitle>
-          <SheetDescription>{description}</SheetDescription>
-          <div>scenario goes here screenshots etc</div>
-        </SheetHeader>
+      <SheetContent className="dark text-white sm:w-[600px] sm:max-w-[1000px] flex flex-col justify-between overflow-auto">
+        <div>
+          <SheetHeader>
+            <SheetTitle>
+              {name} <span>{id}</span>
+            </SheetTitle>
+            <SheetDescription>{description}</SheetDescription>
+            <SheetDescription className="text-base font-semibold dark:text-green-500">
+              Expected Outcome: this should happen
+            </SheetDescription>
+          </SheetHeader>
+          <div className="mt-8">
+            <div>scenario goes here screenshots etc</div>
+            <p>
+              {" "}
+              1. click here <br /> 2. then go here 1. click here <br /> 2. then
+              go here 1. click here <br /> 2. then go here 1. click here <br />{" "}
+              2. then go here 1. click here <br /> 2. then go here 1. click here{" "}
+              <br /> 2. then go here 1. click here <br /> 2. then go here 1.
+              click here <br /> 2. then go here Lorem ipsum dolor sit amet Lorem
+              ipsum dolor sit, amet consectetur adipisicing elit. Ab, eos?
+              <Image src={"/file.svg"} width={200} height={100} alt="tes" />
+              <Image src={"/file.svg"} width={200} height={100} alt="tes" />
+              <Image src={"/file.svg"} width={200} height={100} alt="tes" />
+            </p>
+          </div>
+        </div>
       </SheetContent>
     </Sheet>
   );
